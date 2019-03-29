260a9690d527ec9c036d232ada8b75b3f062d419
Nihal Kumarswamy <kumarnihal04@gmail.com>
2019-03-29 00:12:36 -0500

    fixed ncurses.hpp

--- Practice_Make/chat_client.cpp
+++ Practice_Make/chat_client.cpp
@@ -8,7 +8,7 @@
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
-//#include <ncurses.hpp>
+#include <ncurses.h>
 #include <asio.hpp>
 
 #include <cstdlib>
---
+++ asio_ex/test.cpp
@@ -0,0 +1,7 @@
+#include <asio.hpp>
+#include <iostream>
+
+int main(){
+    asio::io_context io;
+    return 0;
+}
