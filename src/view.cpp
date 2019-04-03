0e4836c8cc1fc7693d87e362023bdf8988b75cd0
Tolulope0 <44985202+Tolulope0@users.noreply.github.com>
2019-04-02 19:41:59 -0500

    working on view class
    debugging a function

--- src/view.cpp
+++ src/view.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include"view.h"
 #include<ncurses.h>
+#include<string.h>
 using namespace std;
 
 View::View(){}
@@ -12,19 +13,48 @@ string View:: fonts(){}
 void View:: loginScreen(){}
 void View:: signUpScreen()
 {
+	 int yMax,xMax,start_y,start_x;
+        getmaxyx(stdscr,yMax,xMax);
+        WINDOW * me = newwin(5,xMax-50,yMax-24,26);
+        //WINDOW * mee = newwin(3,xMax-20,yMax-20,10);
+        WINDOW * labelmee = newwin(3,xMax-20,yMax-17,10);
+        WINDOW * labelmeen = newwin(3,xMax-40,yMax-12,10);
+        WINDOW * mee = newwin(3,xMax-40,yMax-15,10);
+        WINDOW * meen = newwin(3,xMax-40,yMax-10,10);//length,left right, up down, width
+        //box(me,0,0);
 
-	initscr();
-	noecho();
-	int yMax,xMax,start_y,start_x;
-	getmaxyx(stdscr,yMax,xMax);
-	WINDOW * me = newwin(10,xMax-50,yMax-20,5);
-	box(me,0,0);
-	mvwprintw(me,1,1,"Sign Up");
-	refresh();
-	wrefresh(me);
+        mvwprintw(me,1,1,"Sign Up");
+        mvwprintw(labelmee,1,0," Enter Username:");
+        mvwprintw(labelmeen,1,0," Enter Password:");
+        char  username[100];
+        char  password[100];
 
+        box(mee,0,0);
+        box(meen,0,0);
 
+        refresh();
+        wrefresh(me);
+ wrefresh(labelmee);
+        wrefresh(labelmeen);
+        wrefresh(mee);
+        wrefresh(meen);
+
+        mvwgetstr(mee,1,1,username);
+        while (username != "go")//this will be modified and used for when a user enters in a username that has already been taken
+        {
+                mvwprintw(labelmee,1,0," Enter a different user name this has already");
+                wrefresh(labelmee);
+                printw("username: %s",username);
+                memset (username,0,sizeof(username));
+
+                mvwgetstr(mee,1,1,username);
+        }
+        mvwgetstr(meen,1,1,password);
+
+        getch();
+        endwin();
 }
+
 void View:: Welcome()
 {
 	initscr();
@@ -77,14 +107,17 @@ void View:: Welcome()
 
 printw("You chose %s",choices[highlight].c_str());
 string next = choices[highlight];
+clear();
+refresh();
 if (next == choices[0])
 signUpScreen();
 else 
 loginScreen();
 
 getch();
-endwin();
+refresh();
 
+//erase();
 }
 
 string View:: help(){}
