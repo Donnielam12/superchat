a22e260a76ee8272de75db56e75723d3eff9f7ca
Nihal Kumarswamy <kumarnihal04@gmail.com>
2019-04-05 23:37:32 -0500

    cleared lib's

--- asio_ex/test.cpp
+++ asio_ex/test.cpp
@@ -3,5 +3,9 @@
 
 int main(){
     asio::io_context io;
+    tcp::resolver resolver(io_context);
+    auto endpoints = resolver.resolve(argv[1], argv[2]);
+    tcp::socket socket=&io;
+    
     return 0;
 }
